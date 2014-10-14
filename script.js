23a06c5375cc1609325479e233ff092c5bd37988
patmood <patrick.n.moody@gmail.com>
2014-10-13 20:51:06 -0700

    index template

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
-$(document).ready(function(){
-  $('.email-me').on('click',function(){
-    $(this).html('<input type="text" value="patrick.n.moody@gmail.com">');
-    $('.email-me input').select();
-    $(this).unbind('click');
-  });
+// On Ready
+$(function() {
+  // Email button click
+  $('.email-me').on('click', function() {
+    $(this).html('<input type="text" value="patrick.n.moody@gmail.com">')
+    $('.email-me input').select()
+    $(this).unbind('click')
+  })
 
-});
+  // Populate project info
+  // $.getJSON('projects.json', function(data) {
+  //   data.projects.forEach(function(project) {
+  //     $('#projects').append(_.template(projectTemplate, project))
+  //   })
+  // })
+})
+
+// BACKBONE
+var IndexView = Backbone.View.extend({
+  el: '#content'
+, render: function() {
+    var template = _.template($('#index-template').html())
+    this.$el.html(template)
+  }
+})
+
+var ProjectsView = Backbone.View.extend({
+  el: '#content'
+, render: function() {
+    var projects = {}
+    var template = _.template($('#project-template').html(), projects)
+  }
+})
+
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    '': 'index'
+  , 'madebyme': 'projects'
+  }
+, index: function() {
+    new IndexView().render()
+  }
+})
+
+var router = new AppRouter()
+Backbone.history.start()
